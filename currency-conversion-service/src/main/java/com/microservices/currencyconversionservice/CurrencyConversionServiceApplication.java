e97b7816321f2cd583853b4646f70c061b9be3af
Wolverine <Wolverine@DESKTOP-942HOHM>
2020-05-31 00:41:43 +0530

    eureka naming server changes
    Change-Id: I46216158d36ddf7c09b71833c1233dda25bde4f7

--- currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,11 +2,13 @@ package com.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 //@RibbonClient(name ="currency-exchange-service")
 public class CurrencyConversionServiceApplication {
 
